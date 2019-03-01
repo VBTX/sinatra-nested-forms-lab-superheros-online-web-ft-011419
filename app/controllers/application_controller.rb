a02350a49069867357e5418045e5d26ae1965b70
Victoria B <viktorija.bourassa@gmail.com>
2019-03-01 16:42:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     post "/teams" do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
-      @super_heroes = members.collect do |member_params|
+      @team_members = members.collect do |member_params|
         SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
       erb :team
